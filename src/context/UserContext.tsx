c49dde8b4c6082de08e8630e1b85dbc95529a280
Luiz Ricardo <luizricardo41@gmail.com>
2022-10-04 14:36:47 -0300

    inseridos novos estados

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,8 +1,8 @@
-import { GridCellParams } from "@mui/x-data-grid";
 import React,
 {
   createContext, Dispatch, MouseEvent, SetStateAction, useState,
 } from "react";
+import { GridCellParams } from "@mui/x-data-grid";
 
 import { IUserData } from "../interfaces/IUserData";
 
@@ -19,6 +19,8 @@ interface IUsersInfoData {
   setAnchorEl: Dispatch<React.SetStateAction<HTMLElement | null>>;
   userData: IUserData;
   setUserData: Dispatch<React.SetStateAction<IUserData>>;
+  passwordConfirm: string;
+  setPasswordConfirm: Dispatch<React.SetStateAction<string>>;
 };
 
 export const UsersInfo = createContext<IUsersInfoData>({} as IUsersInfoData);
@@ -31,6 +33,9 @@ export const UsersInfoProvider: React.FC<IUserInfoProviderProps> = ({ children }
   const [editUser, setEditUser] = useState(false);
   const [userModal, setUserModal] = useState(false)
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [openEdit, setOpenEdit] = useState(false);
+
   const [userData, setUserData] = useState<IUserData>({
     nome: '',
     email: '',
@@ -38,9 +43,11 @@ export const UsersInfoProvider: React.FC<IUserInfoProviderProps> = ({ children }
     cpf: '',
     telefone: '',
     dataNascimento: '',
+    password: '',
+    endereco: '',
+    cidade: '',
   });
 
-  const [openEdit, setOpenEdit] = useState(false);
 
   const handleClickEditUser = (event: React.MouseEvent<HTMLButtonElement>, index: IUserData) => {
     setEditUser(true);
@@ -71,6 +78,8 @@ export const UsersInfoProvider: React.FC<IUserInfoProviderProps> = ({ children }
         setUserData,
         openEdit,
         setOpenEdit,
+        passwordConfirm,
+        setPasswordConfirm,
       }
     }
     >
