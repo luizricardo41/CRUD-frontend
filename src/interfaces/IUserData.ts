fb7b517896c939b3a3f81badcde964fec3e21022
Luiz Ricardo <luizricardo41@gmail.com>
2022-10-04 23:06:59 -0300

    criação de nova interface

--- src/interfaces/IUserData.ts
+++ src/interfaces/IUserData.ts
@@ -2,6 +2,7 @@ import { GridRenderCellParams, GridValueGetterParams } from "@mui/x-data-grid";
 import { ReactNode } from "react";
 
 export interface IUserData {
+  id?: number;
   nome: string;
   email: string;
   status: string;
@@ -19,4 +20,12 @@ export interface IUserDataProps {
 
 export interface IIndexProps {
   index: IUserData;
+}
+
+export interface IRow {
+  id: number;
+  nome: string;
+  email: string;
+  status: number;
+  cpf: number;
 }
